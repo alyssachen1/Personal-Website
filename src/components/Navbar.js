3477d5bbc3bf3638beee244f1ffe641f8f6801ba
Alyssa <alyssa_chen1@berkeley.edu>
2023-12-24 19:14:12 -0800

    css stuff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import Icon from '../assets/home.png';
-
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
 
 function Navbar() {
   return (
     <div className='navbar'>
-        <div className='leftside'></div>
+        <div className='leftside'>
             <img src={Icon} />
+        </div>
 
-        <div className='rightside'></div>
+        <div className='rightside'>
+            <Link to ='/home'> Home </Link>
+            <Link to ='/menu'> Menu </Link>
+            <Link to ='/about'> About </Link>
+            <Link to ='/contact'> Contact </Link>
+        </div>
     </div>
-  )
+  );
 }
 
 export default Navbar;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      Welcome, I'm Alyssa
+    </div>
+  )
+}
+
+export default Home;
